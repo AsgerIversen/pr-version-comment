0f02549f9dc9debfa1e9f14facf8d85431a98fe3
Asger Iversen <asger_iversen@keysight.com>
2022-01-21 00:52:44 +0100

    Use tap to get version from merge sha

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 using Octokit;
 using System.Collections;
+using System.Diagnostics;
 
 string owner = "AsgerIversen";
 string reponame = "pr-version-comment";
-
 
 if (args.Length > 0)
 {
@@ -22,4 +22,12 @@ var repoid = repo.Id;
 var prs = await github.PullRequest.GetAllForRepository(repoid, new PullRequestRequest { State = ItemStateFilter.Closed });
 Console.WriteLine("Closed Pull Requests:");
 foreach (var pr in prs)
+{
     Console.WriteLine($"  {pr.Id}:{pr.Title}");
+    if(pr.Merged)
+    {
+        Process.Start("tap", $"sdk gitversion {pr.MergeCommitSha}");
+    }
+}
+
+
