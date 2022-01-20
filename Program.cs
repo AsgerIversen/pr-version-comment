75c54eccdbbc52baf8130fe48dbc457b96b6663e
Asger Iversen <asger_iversen@keysight.com>
2022-01-21 00:44:13 +0100

    Try to get closed PRs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,25 @@
 using Octokit;
 using System.Collections;
 
-string owner = args[0];
-string repo = args[1];
-string token = args[2];
+string owner = "AsgerIversen";
+string reponame = "pr-version-comment";
 
+
+if (args.Length > 0)
+{
+    owner = Environment.GetEnvironmentVariable("GITHUB_REPOSITORY_OWNER");
+    reponame = Environment.GetEnvironmentVariable("GITHUB_REPOSITORY");
+    token = args[0];
+}
 Console.WriteLine("Variables:");
 foreach (DictionaryEntry v in Environment.GetEnvironmentVariables())
 Console.WriteLine($"  {v.Key}={v.Value}");
-var github = new GitHubClient(new ProductHeaderValue("LabToHub"));
+var github = new GitHubClient(new ProductHeaderValue("pr-version-comment"));
 github.Credentials = new Credentials(token);
+var repo = await github.Repository.Get(owner, reponame);
+var repoid = repo.Id;
+
+var prs = await github.PullRequest.GetAllForRepository(repoid, new PullRequestRequest { State = ItemStateFilter.Closed });
+Console.WriteLine("Closed Pull Requests:");
+foreach (var pr in prs)
+    Console.WriteLine($"  {pr.Id}:{pr.Title}");
