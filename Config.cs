48b8e8c5690fdde2837ac7a0e5cae3e7aa7e4747
Asger Iversen <asger_iversen@keysight.com>
2022-01-22 23:43:13 +0100

    Initial linked issues work

---
+++ Config.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRVersionComment
+{
+    internal static class Config
+    {
+        public static string Owner { get;set;} = "AsgerIversen";
+        public static string RepoName { get; set; } = "pr-version-comment";
+        public static string Token { get; set; } = "";
+        public static string Body { get; set; } = "This change is part of version `{version}` or later.";
+        public static string IssueBody { get; set; } = "A fix for this is in version `{version}` or later.";
+    }
+}
